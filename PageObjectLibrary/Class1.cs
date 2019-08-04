bb20335defa7fe1f6ac650cde60f251d6c6601bf
axtherlov <47370422+axtherlov@users.noreply.github.com>
2019-08-04 19:56:05 -0400

    guia tarea

--- PageObjectLibrary/Class1.cs
+++ PageObjectLibrary/Class1.cs
@@ -8,5 +8,18 @@ namespace PageObjectLibrary
 {
     public class Class1
     {
+            //guia para la tarea
+
+            //Random rnd = new Random();
+            //int index = rnd.Next(4); //0-4
+            //IList<IWebElement> products = webDriver.FindElements(By.XPath("//div[@class='button-container']/a[@class='button ajax_add_to_cart_button btn btn-default']/span"));
+            //IWebElement productCell = products[index];
+            //actions.MoveToElement(productCell);
+            //productCell.Click();
+
+            ////div[@class='button-container']/a[@class='btn btn-default button button-medium']/span
+
+            //IWebElement checkbox = webDriver.FindElement(By.Id("cgv"));
+            //checkbox.Click();
     }
 }
